d4cf5042576897d2935fbd51be22aeb876072c43
Amr Mesbah <amr.mesbah@future-group.com>
2025-03-04 15:33:55 +0200

    list customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -20,7 +20,7 @@ async function Page(props: {
 }) {
     const searchParams = await props.searchParams;
     const query = searchParams?.query ?? '';
-    const currentPage = Number(searchParams?.page) ?? 1;
+    const currentPage = Number(searchParams?.page) || 1;
 
     const totalPages = await fetchCustomersPages(query);
 
